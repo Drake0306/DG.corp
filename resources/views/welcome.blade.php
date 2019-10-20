a6d631c38c0eb9b88319f79f24bd496c53105997
Abhinav Roy <danroy48@gmail.com>
2019-10-20 11:57:07 +0530

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="{{asset('mdb/css/bootstrap.css')}}">
+<link rel="stylesheet" href="{{asset('mdb/css/animations-extended.css')}}">
+<link rel="stylesheet" href="{{asset('mdb/css/datatables.css')}}">
+<link rel="stylesheet" href="{{asset('mdb/css/datatables-select.css')}}">
+<script src="{{asset('mdb/js/jquery-3.4.1.min.js')}}"></script>
+<script src="{{asset('mdb/js/bootstrap.js')}}"></script>
+<script src="{{asset('mdb/js/addons/datatables.js')}}"></script>
+<script src="{{asset('mdb/js/addons/datatables-select.js')}}"></script>
+<script src="{{asset('mdb/js/addons/progressBar.js')}}"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+</head>
+<body>
+    
+</body>
+</html>
